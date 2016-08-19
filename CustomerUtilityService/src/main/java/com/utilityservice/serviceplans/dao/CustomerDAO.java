f0416778865264da753e7020957b27beedf4284b
uday <uvandanapu.bcj@gmail.com>
2016-08-19 02:25:25 -0500

    log4j , datasource

--- CustomerUtilityService/src/main/java/com/utilityservice/serviceplans/dao/CustomerDAO.java
+++ CustomerUtilityService/src/main/java/com/utilityservice/serviceplans/dao/CustomerDAO.java
@@ -3,8 +3,6 @@ package com.utilityservice.serviceplans.dao;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -13,8 +11,7 @@ import com.utilityservice.serviceplans.model.Customer;
 @Repository
 public class CustomerDAO {
 
-	private static final Logger logger = LoggerFactory.getLogger(CustomerDAO.class);
-
+	
 	@Autowired
 	private SessionFactory sessionFactory;
 
@@ -25,7 +22,7 @@ public class CustomerDAO {
 		
 	int id = (Integer) session.save(customer);
 		
-		logger.info("Person saved successfully, Person Details=" + customer);
+	
 		
 		
 		return  id;
--- WS-Provider/src/main/java/com/bootcamp/serviceprovider/utility/HibernateUtility.java
+++ WS-Provider/src/main/java/com/bootcamp/serviceprovider/utility/HibernateUtility.java
@@ -1,27 +1,44 @@
 package com.bootcamp.serviceprovider.utility;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
 
 @SuppressWarnings("deprecation")
 public class HibernateUtility {
 
 	private static final SessionFactory sessionFactory;
+	private static Session session;
+
 	static {
 		try {
 			sessionFactory = new Configuration().configure().buildSessionFactory();
-			
+
 		} catch (Throwable exception) {
-			
+
 			System.err.println("Initial SessionFactory creation failed." + exception);
-			
+
 			throw new ExceptionInInitializerError(exception);
 		}
 	}
 
-	public static SessionFactory getSessionFactory() {
-		
-		return sessionFactory;
+	public static Session getSession() {
+
+		if (session == null) {
+
+			session = sessionFactory.openSession();
+		}
+		return session;
 	}
+
+	public static Session closeSession() {
+
+		if (session != null) {
+			session.close();
+			session = null;
+		}
+
+		return session;
+	}
+
 }
